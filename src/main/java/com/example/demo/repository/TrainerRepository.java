d6fdd6f6057364cd697982e8b493d7694e190af7
s555842 <satyadeepu.andapati@gmail.com>
2023-11-14 14:54:05 -0600

    update1

---
+++ src/main/java/com/example/demo/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.Models.Trainer;
+
+/**
+ * @author Satya Deepu Mandapati
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
+
